479c4387d7b572fad39737cbd4b805a355cc983b
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-10-23 16:03:58 +0000

    I don't think this works.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+let todos = ['first todo', 'second todo', 'third todo'];
+let i = 0;
 
+function markAsDone(todos){
+    todos.foreach(todos => 'done - ' + todos[i], i++);
+    return todos;
+}
+
+document.getElementById('result').innerHTML = todos;
