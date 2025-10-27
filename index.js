d067eea7bddd524b4d50b44df40f321edc6c6c25
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-10-27 18:14:32 +0000

    Random Commit

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ let todos = ['first todo', 'second todo', 'third todo'];
 let i = 0;
 
 function markAsDone(todos){
-    if(todos.length > i){
-        i++
-    }
     todos.foreach(todos1 => 'done - ' + todos[i]);
     return todos;
 }
