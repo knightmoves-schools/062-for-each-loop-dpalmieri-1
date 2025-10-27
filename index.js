c694b3def23c3e04e446f3e96a72034459336f16
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-10-27 17:57:47 +0000

    Foreach

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let todos = ['first todo', 'second todo', 'third todo'];
 let i = 0;
 
 function markAsDone(todos){
-    todos.foreach(todos => 'done - ' + todos[i], i++);
+    foreach(todos => 'done - ' + todos);
     return todos;
 }
 
